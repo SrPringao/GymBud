5042424eb0db374c3996374b3acf1fc73a3af6b2
SrPringao <franco.aldrete@GMAIL.COM>
2023-01-04 14:59:52 -0600

    Pre de elementos pantalla login

--- app/src/main/java/com/example/gymbud/MainActivity.java
+++ app/src/main/java/com/example/gymbud/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gymbud;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void LinkRegistrarse(View view) {
+
     }
 }
