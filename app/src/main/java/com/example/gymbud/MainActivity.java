39b1ab6ce2ee03f2f59786f819ddb550c2f6f90c
SrPringao <franco.aldrete@GMAIL.COM>
2023-01-06 15:25:45 -0600

    soy un pelotudo y pense que la funcion de onclick que habia era para iniciar sesion, pero ya puse la otra namas que no se pq no jala el inicio de sesion, segun yo deberia de funcionar al tiro pq ya le puse el link del dominio pero de seguro en algo la estoy cagando, hasta aki mi report juakin

--- app/src/main/java/com/example/gymbud/MainActivity.java
+++ app/src/main/java/com/example/gymbud/MainActivity.java
@@ -26,17 +26,20 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 EditText ETusr,ETcontra;
-Button BIngreso;
+Button btnIngreso;
 TextView TVRegistro, TVRecuperar;
+SharedPreferences archivo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ETusr = findViewById(R.id.etUsuario);
         ETcontra = findViewById(R.id.etContraseña);
-        BIngreso = findViewById(R.id.botonIngresar);
+        btnIngreso = findViewById(R.id.botonIngresar);
         TVRegistro = findViewById(R.id.TVRegistro);
         TVRecuperar = findViewById(R.id.TVRecuperar);
+
 
         TVRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,5 +92,42 @@ TextView TVRegistro, TVRecuperar;
             }
         });
         lanzarPeticion.add(pet);
+    }
+
+    public void clickInicio(View view) {
+        String url = "http://francoaldrete.com/GymBud/bd.php?usr=";
+        url = url + ETusr.getText().toString();
+        url = url + "&pass=";
+        url = url + ETcontra.getText().toString();
+
+        JsonObjectRequest pet = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if (response.getInt("usr") != -1) {
+                        Intent i = new Intent(MainActivity.this, MainActivity.class);
+                        SharedPreferences.Editor editor = archivo.edit();
+                        editor.putInt("id_usuario", response.getInt("usr"));
+                        editor.commit();
+                        startActivity(i);
+                        finish();
+                    } else {
+                        ETusr.setText("");
+                        ETcontra.setText("");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(MainActivity.this,response.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("yo", error.getMessage());
+            }
+        });
+        RequestQueue lanzarPeticion= Volley.newRequestQueue(this);
+        lanzarPeticion.add(pet);
+        lanzarPeticion.start();
     }
 }
