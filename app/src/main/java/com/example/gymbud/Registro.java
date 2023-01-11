4f8a7200176923465a2ae4986c5237a3cb1a413e
SrPringao <franco.aldrete@GMAIL.COM>
2023-01-11 14:32:07 -0600

    php de verificacion de usuario y antes de implementarlo en la app

--- app/src/main/java/com/example/gymbud/Registro.java
+++ app/src/main/java/com/example/gymbud/Registro.java
@@ -1,7 +1,5 @@
 package com.example.gymbud;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,7 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.util.Patterns;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -22,9 +22,14 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+
 public class Registro extends AppCompatActivity {
-    EditText ETusuario,ETcontrasena,ETcontrasenaconf,ETcorreo;
+    EditText ETusuario, ETcontrasena, ETcontrasenaconf, ETcorreo;
     Button Bconfirmar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +43,7 @@ public class Registro extends AppCompatActivity {
 
     }
 
-    public void confirmar (View view){
+    public void confirmar(View view) {
 
 
         ProgressDialog progressDialog = new ProgressDialog(this);
@@ -49,66 +54,69 @@ public class Registro extends AppCompatActivity {
         final String con1 = ETcontrasena.getText().toString().trim();
         final String con2 = ETcontrasenaconf.getText().toString().trim();
 
-        if(usuario.isEmpty()){
+        if (usuario.isEmpty()) {
             Toast.makeText(Registro.this, "Ingrese su nombre de usuario", Toast.LENGTH_SHORT).show();
             return;
-        }else if(correo.isEmpty()){
+        } else if (correo.isEmpty()) {
             Toast.makeText(Registro.this, "Ingrese su correo electronico", Toast.LENGTH_SHORT).show();
             return;
-        }else if(con1.isEmpty()){
+        } else if (con1.isEmpty()) {
             Toast.makeText(Registro.this, "Ingrese su contraseña", Toast.LENGTH_SHORT).show();
             return;
-        }else if(con2.isEmpty()){
+        } else if (con2.isEmpty()) {
             Toast.makeText(Registro.this, "Ingrese la confirmacion de su contraseña", Toast.LENGTH_SHORT).show();
             return;
-        }else if (!TextUtils.equals(con1,con2)){
+        } else if (!TextUtils.equals(con1, con2)) {
             Toast.makeText(Registro.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
             return;
-        }else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
             Toast.makeText(Registro.this, "Ingresa un correo valido", Toast.LENGTH_SHORT).show();
             return;
         }
 
         progressDialog.show();
 
+//        String urlv = "http://francoaldrete.com/GymBud/verificacion.php?usr=";
+//        urlv += usuario;
+//        JsonObjectRequest peticion = new JsonObjectRequest(Request.Method.GET, urlv, null, new Response.Listener<JSONObject>()
 
 
         String url = "http://francoaldrete.com/GymBud/insert.php?usr=";
-            url = url + ETusuario.getText().toString();
-            url = url + "&mail=";
-            url = url + ETcorreo.getText().toString();
-            url = url + "&pass=";
-            url = url + ETcontrasena.getText().toString();
-            JsonObjectRequest pet = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    try {
-                        if (response.getInt("") != -1) {
-                            Intent i = new Intent(Registro.this, MainActivity.class);
-                            startActivity(i);
-                            finish();
-                        }
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+        url = url + ETusuario.getText().toString();
+        url = url + "&mail=";
+        url = url + ETcorreo.getText().toString();
+        url = url + "&pass=";
+        url = url + ETcontrasena.getText().toString();
+        JsonObjectRequest pet = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    if (response.getInt("") != -1) {
+                        Intent i = new Intent(Registro.this, MainActivity.class);
+                        startActivity(i);
+                        finish();
                     }
 
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("yo", error.getMessage());
-                }
-            });
-            RequestQueue lanzarPeticion= Volley.newRequestQueue(this);
-            lanzarPeticion.add(pet);
-            lanzarPeticion.start();
-            progressDialog.dismiss();
-            Toast.makeText(Registro.this, "Te has registrado exitosamente", Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(Registro.this, MainActivity.class);
-            startActivity(i);
-            finish();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("yo", error.getMessage());
+            }
+        });
+        RequestQueue lanzarPeticion = Volley.newRequestQueue(this);
+        lanzarPeticion.add(pet);
+        lanzarPeticion.start();
+        progressDialog.dismiss();
+        Toast.makeText(Registro.this, "Te has registrado exitosamente", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(Registro.this, MainActivity.class);
+        startActivity(i);
+        finish();
 
 
     }
