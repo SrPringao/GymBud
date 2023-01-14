8dcc83c47db0696bb33edf945aa3f2bc8f8b12c3
SrPringao <franco.aldrete@GMAIL.COM>
2023-01-13 19:30:36 -0600

    antes de añadir los fragments

---
+++ app/src/main/java/com/example/gymbud/FragmentContainer.java
@@ -0,0 +1,14 @@
+package com.example.gymbud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentContainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_container);
+    }
+}
