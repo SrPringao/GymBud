55186f9d419d7f480a714c78ef2cf25868123563
FranqZD <a19300095@ceti.com.mx>
2023-01-15 00:42:32 -0600

    Arregle lo del arroba y los caracteres especiales

--- app/src/main/java/com/example/gymbud/MainActivity.java
+++ app/src/main/java/com/example/gymbud/MainActivity.java
@@ -83,9 +83,9 @@ SharedPreferences archivo;
         //Toast.makeText(this, "picon",Toast.LENGTH_SHORT).show();
 
         String url = "http://francoaldrete.com/GymBud/bd.php?usr=";
-        url = url + ETusr.getText().toString();
+        url = url + Uri.encode(ETusr.getText().toString());
         url = url + "&pass=";
-        url = url + ETcontra.getText().toString();
+        url = url + Uri.encode(ETcontra.getText().toString());
 
         //este toast es pa verificar que el url se cree bien
 
