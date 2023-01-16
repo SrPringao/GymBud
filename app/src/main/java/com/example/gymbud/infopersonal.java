6c3b887e3f5700f04bc66e330e9e18fa9a51ddd4
SrPringao <franco.aldrete@GMAIL.COM>
2023-01-15 22:37:05 -0600

    X scroll

--- app/src/main/java/com/example/gymbud/infopersonal.java
+++ app/src/main/java/com/example/gymbud/infopersonal.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import eightbitlab.com.blurview.BlurView;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link infopersonal#newInstance} factory method to
@@ -19,6 +21,7 @@ public class infopersonal extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
