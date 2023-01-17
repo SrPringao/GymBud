8608c857c15b04408438176b97ab9a177a2144ab
SrPringao <franco.aldrete@GMAIL.COM>
2023-01-17 14:18:59 -0600

    x ranciovich

--- app/src/main/java/com/example/gymbud/infopersonal.java
+++ app/src/main/java/com/example/gymbud/infopersonal.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import eightbitlab.com.blurview.BlurView;
 
@@ -16,6 +17,9 @@ import eightbitlab.com.blurview.BlurView;
  * create an instance of this fragment.
  */
 public class infopersonal extends Fragment {
+
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -49,12 +53,16 @@ public class infopersonal extends Fragment {
         return fragment;
     }
 
+    ScrollView scroll;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+            scroll = scroll.findViewById(R.id.scroll);
         }
     }
 
