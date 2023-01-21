893450595a9619579c1aca10a73015c5d7255126
FranqZD <a19300095@ceti.com.mx>
2023-01-20 19:48:24 -0600

    Query bien

--- app/src/main/java/com/example/gymbud/MainActivity.java
+++ app/src/main/java/com/example/gymbud/MainActivity.java
@@ -20,7 +20,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.gymbud.db.DbHelper;
 import com.example.gymbud.db.DbQuery;
 import com.example.gymbud.db.Entidades.PersonInfo;
 
