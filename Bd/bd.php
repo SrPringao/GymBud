c39e86ab17db078c29f4298bc1a2ce07b5315b6b
SrPringao <franco.aldrete@GMAIL.COM>
2023-04-14 15:17:28 -0600

    Añadi los archivos buenos de la nube

--- Bd/bd.php
+++ Bd/bd.php
@@ -4,19 +4,19 @@ if ($_SERVER['REQUEST_METHOD']=='GET')
     require_once("conexion.php");
     $id = $_GET["usr"];
     $pass =$_GET["pass"];
-
     $query = "SELECT * FROM user WHERE  User= '$id' AND Password = '$pass'";
-    $result = $mysqli ->query($query);
+    $result = $link ->query($query);
 
-    if ($mysqli -> affected_rows > 0) {
+    if ($link -> affected_rows > 0) {
         while ($row = $result ->fetch_assoc()) 
         {
             $array = $row;
         }
-        echo json_encode($array);
+       echo json_encode($array);
     }else {
-        echo "Not found any rows";
+        
+		echo "Not found any rows";
     }
     $result ->close();
-    $mysqli ->close();
+    $link ->close();
 }
--- Bd/insert.php
+++ Bd/insert.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD']=='GET')
  $mail =$_GET["mail"];
  $pass =$_GET["pass"];
 
- $query ="INSERT INTO user VALUES('4','$user','$mail','$pass','0')";
+ $query ="INSERT INTO user VALUES(NULL,'$user','$mail','$pass','0')";
 if(!mysqli_query($link,$query));
   die(printf("<H3>Data record 1 cannot be inserted: [%d] %s</H3>", mysqli_connect_errno(), mysqli_connect_error())); 
  $mysqli ->close();
